d3bb1a1c64218b132225e5faf2f459a3709eeff8
Blessing <bntoko@gmail.com>
2021-07-24 00:20:56 +0200

    added js for sticky navbar

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,12 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('DOM Content Loaded');
 });
 
-let nav = document.getElementById('navigate');
+const nav = document.getElementById('navigate');
 
 document.addEventListener('scroll', () => {
-    console.log('scrolling my way downtown');
     if (window.scrollY) {
-        console.log('Adding black?');
         nav.classList.add('black');
     } else {
         nav.classList.remove('black');
