fe63482bcd3336f89139b5491105791ed32b4588
Blessing <bntoko@gmail.com>
2021-10-10 10:08:32 +0200

    added logo to website; edited some content and class names in html file; edited js file

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,11 +3,14 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 const nav = document.getElementById('navigate');
+const letterN = document.getElementById('changeColour');
 
 document.addEventListener('scroll', () => {
     if (window.scrollY) {
         nav.classList.add('black');
+        letterN.classList.add('white');
     } else {
         nav.classList.remove('black');
+        letterN.classList.remove('white');
     }
 });
